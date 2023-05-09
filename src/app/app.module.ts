9f8415e9fc75455491693deeb93c4251f9c5e8a6
sergeys <sjeneyev@gmail.com>
2023-05-09 10:21:15 +0300

    starting recipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,12 @@ import { SharedModule } from './shared/shared.module';
 import { RecipesComponent } from './recipes/recipes.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { RecipeCategoryComponent } from './recipes/recipe-category/recipe-category.component';
+import { RecipeComponent } from './recipes/recipe/recipe.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, RecipesComponent],
+  declarations: [AppComponent, HeaderComponent, RecipesComponent, RecipeCategoryComponent, RecipeComponent, RecipeEditComponent],
   imports: [
     AppRoutingModule,
     BrowserModule,
---
+++ src/app/recipes/recipe-category/recipe-category.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-category',
+  templateUrl: './recipe-category.component.html',
+  styleUrls: ['./recipe-category.component.scss']
+})
+export class RecipeCategoryComponent {
+
+}
