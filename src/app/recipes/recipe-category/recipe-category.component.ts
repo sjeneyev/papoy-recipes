deb0cdc86b509496b43531808908555a4ba3f9fa
sergeys <sjeneyev@gmail.com>
2023-05-21 09:14:37 +0300

    commit

--- src/app/recipes/recipe-category/recipe-category.component.ts
+++ src/app/recipes/recipe-category/recipe-category.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ICategory } from '../../interfaces/interfaces';
 
 @Component({
   selector: 'app-recipe-category',
   templateUrl: './recipe-category.component.html',
-  styleUrls: ['./recipe-category.component.scss']
+  styleUrls: ['./recipe-category.component.scss'],
 })
 export class RecipeCategoryComponent {
-
+  @Input() recipe: ICategory;
 }
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ICategory } from '../interfaces/interfaces';
+import { environment as env } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.scss'],
 })
-export class RecipesComponent {
-  testArray = [1, 2, 3, 4, 5, 6, 7, 8];
+export class RecipesComponent implements OnInit {
+  categories = [];
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http
+      .get<ICategory[]>(`${env.dataEndpoint}/categories.json`)
+      .subscribe((response) => {
+        this.categories = response;
+      });
+  }
 }
